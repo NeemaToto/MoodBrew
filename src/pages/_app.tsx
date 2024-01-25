93173aea90f7764eb33bb843298956153d7190c7
Neema Totonchi <ntotonchi1@se14l0120-15.ad.bcit.ca>
2024-01-25 12:39:30 -0800

    add mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import '@mantine/core/styles.css';
+import { createTheme, MantineProvider } from '@mantine/core';
+
+const theme = createTheme({
+
+});
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider theme={theme}>
+  <Component {...pageProps} />
+  </MantineProvider>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
-
+import { Button } from '@mantine/core'
 
 export default function Home() {
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
-      
+      <Button>Button</Button>
+     <p style={{fontFamily: 'Satoshi'}}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati nulla neque unde reiciendis voluptate itaque dolorem saepe excepturi? Dolor sint, ipsam asperiores aut natus id recusandae nemo ad accusantium quo.</p>
     </main>
   )
 }
