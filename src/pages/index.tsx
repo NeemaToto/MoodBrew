e899619415ccec1bed178ef2af29d757b5ffc871
Patrickhuiwork <patrick.hui.work@gmail.com>
2024-02-04 14:37:50 -0800

    I give up setting up da theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import { Button, Paper, Text, Loader, MantineProvider  } from '@mantine/core'
+import { Button} from '@mantine/core'
 
 export default function Home() {
   return (
       <main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
-        <Button color="blue">Button</Button>
+        <Button  style={{fontFamily: 'Satoshi',  backgroundColor: 'var(--taupe)', fontSize: 'var(--md)', fontWeight: 'var(--bold)'}}>Button</Button>
       <p style={{fontFamily: 'Satoshi'}}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati nulla neque unde reiciendis voluptate itaque dolorem saepe excepturi? Dolor sint, ipsam asperiores aut natus id recusandae nemo ad accusantium quo.</p>
       </main>
   )
