1b82c7412a506b60735a422d158fcdbaf864dd0c
Patrickhuiwork <patrick.hui.work@gmail.com>
2024-02-04 12:10:14 -0800

    trying to setup the theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,43 @@ import '@mantine/core/styles.css';
 import { createTheme, MantineProvider } from '@mantine/core';
 
 const theme = createTheme({
-
+  
 });
+
+
+/*fontFamily: 'Satoshi, sans-serif',
+primaryColor: 'taupe',
+colors: {
+  sage: ['rgba(181, 201, 127, 1)'],
+  taupe: ['rgba(63, 57, 53, 1)'],
+  dun: ['rgba(230, 204, 178, 1)'],
+  seasalt: ['rgba(247, 247, 249, 1)'],
+},
+fontSizes: {
+  sm: '0.78rem',
+  md: '1rem',
+  lg: '1.33rem',
+  xl: '2.33rem',
+  display: '3.56rem',
+  h1:'2.33rem',
+  h2: '2rem',
+  h3: '1.33rem'
+},
+fontWeights: {
+  light: 300,
+  normal: 400,
+  medium: 500,
+  semibold: 600,
+  bold: 700,
+  extrabold: 800,
+  black: 900,
+}*/
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider theme={theme}>
-  <Component {...pageProps} />
-  </MantineProvider>
+    <Component {...pageProps} />
+    </MantineProvider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,8 @@ import { Button } from '@mantine/core'
 export default function Home() {
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
-      <Button>Button</Button>
-     <p style={{fontFamily: 'Satoshi'}}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati nulla neque unde reiciendis voluptate itaque dolorem saepe excepturi? Dolor sint, ipsam asperiores aut natus id recusandae nemo ad accusantium quo.</p>
+      <Button color="blue">Button</Button>
+     <p style={{fontFamily: 'Satoshi', fontWeight: '300'}}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati nulla neque unde reiciendis voluptate itaque dolorem saepe excepturi? Dolor sint, ipsam asperiores aut natus id recusandae nemo ad accusantium quo.</p>
     </main>
   )
 }
