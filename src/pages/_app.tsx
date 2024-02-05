a6c8396e3d3104652d9a721551edeb0fb947ec02
Neema Totonchi <neematotonchi@Neemas-MacBook-Pro.local>
2024-02-05 15:51:23 -0800

    created base theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,48 +1,51 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import '@mantine/core/styles.css';
-import { createTheme, MantineProvider } from '@mantine/core';
+import { createTheme, MantineProvider, rem } from '@mantine/core';
 
-const theme = {
+const theme = createTheme({
+  primaryColor: "sage",
+  colors: {
+    sage: ["#f5fae7", "#ebf2d9", "#d7e1b7", "#c0d191", "#adc371", "#a1ba5d", "#9bb651", "#86a040", "#778f37", "#647b28"],
+    taupe: ["#f6f5f4", "#e7e7e7", "#cdcdcd", "#b1b1b1", "#9b9b9b", "#8c8b8a", "#888481", "#75716e", "#696460", "#5e554f"],
+    dun: ["#fff4e8", "#f5e7d8", "#e7cdb4", "#d8b28b", "#cd9a69", "#c58b53", "#c28446", "#ab7138", "#99652e", "#865624"],
+    seasalt: ["#f3f3f6", "#e4e4e6", "#c7c7cc", "#a8a8b5", "#8d8da0", "#7c7c93", "#74748e", "#62627b", "#57566f", "#4a4a63"]
+  },
+  fontFamily: 'Satoshi, sans-serif',
+  headings: {
+    sizes: {
+      h1: {
+        fontSize: '3.56rem',
+        fontWeight: '700',
+      },
+      h2: {
+        fontSize: '2.33rem',
+        fontWeight: '500',
+      },
+      h3: {
+        fontSize: '2rem',
+        fontWeight: '500',
+      },
+      h4: {
+        fontSize: '1.33rem',
+        fontWeight: '500',
+      },
 
-};
-
-
-
-/*idk how to make this work
-fontFamily: 'Satoshi, sans-serif',
-primaryColor: 'taupe',
-colors: {
-  sage: ['rgba(181, 201, 127, 1)'],
-  taupe: ['rgba(63, 57, 53, 1)'],
-  dun: ['rgba(230, 204, 178, 1)'],
-  seasalt: ['rgba(247, 247, 249, 1)'],
-},
-fontSizes: {
-  sm: '0.78rem',
-  md: '1rem',
-  lg: '1.33rem',
-  xl: '2.33rem',
-  display: '3.56rem',
-  h1:'2.33rem',
-  h2: '2rem',
-  h3: '1.33rem'
-},
-fontWeights: {
-  light: 300,
-  normal: 400,
-  medium: 500,
-  semibold: 600,
-  bold: 700,
-  extrabold: 800,
-  black: 900,
-}*/
-
+    }
+  },
+  fontSizes: {
+    xs: rem(10),
+    sm: rem(14),
+    md: rem(18),
+    lg: rem(24),
+    xl: rem(20),
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider theme={theme}>
-    <Component {...pageProps} />
+      <Component {...pageProps} />
     </MantineProvider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Button} from '@mantine/core'
+import { Button, Text} from '@mantine/core'
 
 export default function Home() {
   return (
       <main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
-        <Button  style={{fontFamily: 'Satoshi',  backgroundColor: 'var(--taupe)', fontSize: 'var(--md)', fontWeight: 'var(--bold)'}}>Button</Button>
-      <p style={{fontFamily: 'Satoshi'}}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati nulla neque unde reiciendis voluptate itaque dolorem saepe excepturi? Dolor sint, ipsam asperiores aut natus id recusandae nemo ad accusantium quo.</p>
+        <Button size='sm'>Button</Button>
+       <Text size='sm'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati nulla neque unde reiciendis voluptate itaque dolorem saepe excepturi? Dolor sint, ipsam asperiores aut natus id recusandae nemo ad accusantium quo.</Text>
       </main>
   )
 }
