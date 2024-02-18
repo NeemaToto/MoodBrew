d3bb6f4c94598cd769bb615bbb7a47a4f6159175
Patrickhuiwork <patrick.hui.work@gmail.com>
2024-02-18 13:46:18 -0800

    fixed text overflow and filled in the content

--- src/components/FAQ/index.tsx
+++ src/components/FAQ/index.tsx
@@ -8,111 +8,185 @@ export default function FAQ() {
   const isSmallScreen = useMediaQuery('(max-width: 768px)');
   
   return (
-      <div className={classes.faq}>
-        <Flex 
-          style={{ display: 'flex' }}
-          direction="column"
+    <div className={classes.faq}>
+      <Flex
+        style={{ display: "flex" }}
+        direction="column"
+        align="center"
+        justify="center"
+        gap={isSmallScreen ? rem(24) : rem(64)}
+        pt={rem(80)}
+        pb={isSmallScreen ? rem(40) : rem(80)}
+      >
+        <Stack
+          gap={rem(0.5)}
           align="center"
           justify="center"
-          gap={isSmallScreen ? rem(24) :rem(64)}
-          pt={rem(80)}
-          pb = {isSmallScreen ? rem(40) : rem(80)}
-          >
-          <Stack gap= {rem(0.5)} align="center" justify="center" style={{ textAlign: 'center', width: '80%' }} >
-            <Text c="taupe" size='md'  fw={700}>
-              Support
+          style={{ textAlign: "center", width: "80%" }}
+        >
+          <Text c="taupe" size="md" fw={700}>
+            Support
+          </Text>
+          <Stack>
+            <Title c="taupe" order={2} fw={700}>
+              Frequently asked
+            </Title>
+            <Text c="taupe" size={isSmallScreen ? "sm" : "md"}>
+              Explore our FAQs to learn more about how MoodBrew personalizes tea
+              recommendations for your mood and health.
             </Text>
-            <Stack >
-              <Title c="taupe" order={2}  fw={700}>
-                Frequently asked
-              </Title>
-              <Text c="taupe"  size = {isSmallScreen ? "sm" : "md"}>
-                Explore our FAQs to learn more about how MoodBrew personalizes tea recommendations for your mood and health.
-              </Text>     
-            </Stack>
           </Stack>
-          <Accordion
-            classNames={{ chevron: classes.chevron }}
-            chevron={<IconPlus className={classes.icon} />}
+        </Stack>
+        <Accordion
+          classNames={{ chevron: classes.chevron }}
+          chevron={
+            <IconPlus
+              className={classes.icon}
+              style={{ color: "var(--taupe)" }}
+            />
+          }
+        >
+          <Flex
+            gap={isSmallScreen ? 0 : 64}
+            direction={isSmallScreen ? "column" : "row"}
           >
-            <Flex
-              gap={isSmallScreen ? 0 : 64}
-              direction={isSmallScreen ? 'column' : 'row'}
-            >
-              <Stack>
-                <Accordion.Item value="what">
-                  <Accordion.Control
-                    icon={
-                      <IconMug
-                        style={{
-                          color: "var(--taupe)",
-                          width: rem(20),
-                          height: rem(20),
-                        }}
-                      />
-                    }
-                  >
+            <Stack>
+              <Accordion.Item value="what">
+                <Accordion.Control
+                  icon={
+                    <IconMug
+                      style={{
+                        color: "var(--taupe)",
+                        width: rem(20),
+                        height: rem(20),
+                      }}
+                    />
+                  }
+                >
+                  <Text fw={500} c="taupe" pr="md">
                     What Is MoodBrew?
-                  </Accordion.Control>
-                  <Accordion.Panel>MoodBrew is a appMoodBrew is a appsssssssssssssssssssssssssssssssssssssssMoodBrew is a appsssssssssssssssssssssssssssssssssssssssMoodBrew is a appsssssssssssssssssssssssssssssssssssssssMoodBrew is a appsssssssssssssssssssssssssssssssssssssss</Accordion.Panel>
-                </Accordion.Item>
+                  </Text>
+                </Accordion.Control>
+                <Accordion.Panel
+                  style={{
+                    maxWidth: "600px",
+                    overflow: "auto",
+                    paddingLeft: "2rem",
+                    paddingright: "3rem",
+                  }}
+                >
+                  MoodBrew is a revolutionary platform designed to personalize
+                  and enhance your tea experience based on your unique
+                  preferences and mood.
+                </Accordion.Panel>
+              </Accordion.Item>
 
-                <Accordion.Item value="recommend">
-                  <Accordion.Control
-                    icon={
-                      <IconTeapot
-                        style={{
-                          color: "var(--taupe)",
-                          width: rem(20),
-                          height: rem(20),
-                        }}
-                      />
-                    }
-                  >
-                    What Should I Do If I Don’t Like A Tea Recommended By MoodBrew?
-                  </Accordion.Control>
-                  <Accordion.Panel>Content</Accordion.Panel>
-                </Accordion.Item>
-              </Stack>
-              <Stack>
-                <Accordion.Item value="how">
-                  <Accordion.Control
-                    icon={
-                      <IconDatabaseSearch
-                        style={{
-                          color: "var(--taupe)",
-                          width: rem(20),
-                          height: rem(20),
-                        }}
-                      />
-                    }
-                  >
+              <Accordion.Item value="recommend">
+                <Accordion.Control
+                  icon={
+                    <IconTeapot
+                      style={{
+                        color: "var(--taupe)",
+                        width: rem(20),
+                        height: rem(20),
+                      }}
+                    />
+                  }
+                >
+                  <Text fw={500} c="taupe" pr="md">
+                    What Should I Do If I Don’t Like A Tea Recommended By
+                    MoodBrew?
+                  </Text>
+                </Accordion.Control>
+                <Accordion.Panel
+                  style={{
+                    maxWidth: "600px",
+                    overflow: "auto",
+                    paddingLeft: "2rem",
+                    paddingright: "3rem",
+                  }}
+                >
+                  If a tea recommended by MoodBrew doesn't suit your taste,
+                  explore alternative options from their diverse selection to
+                  find one that aligns better with your preferences. Providing
+                  feedback to MoodBrew about your experience can help refine
+                  future recommendations and improve the platform's accuracy.
+                  Experimenting with different brewing methods and engaging with
+                  MoodBrew's educational resources can also enhance your
+                  tea-drinking journey and help you discover new favorites.
+                </Accordion.Panel>
+              </Accordion.Item>
+            </Stack>
+            <Stack>
+              <Accordion.Item value="how">
+                <Accordion.Control
+                  icon={
+                    <IconDatabaseSearch
+                      style={{
+                        color: "var(--taupe)",
+                        width: rem(20),
+                        height: rem(20),
+                      }}
+                    />
+                  }
+                >
+                  <Text fw={500} c="taupe" pr="md">
                     How Does MoodBrew Work?
-                  </Accordion.Control>
-                  <Accordion.Panel>Content</Accordion.Panel>
-                </Accordion.Item>
+                  </Text>
+                </Accordion.Control>
+                <Accordion.Panel
+                  style={{
+                    maxWidth: "600px",
+                    overflow: "auto",
+                    paddingLeft: "2rem",
+                    paddingright: "3rem",
+                  }}
+                >
+                  MoodBrew operates by capturing your emotional state and
+                  preferences, then offering sustainable tea recommendations
+                  tailored to address them. With a vast database comprising
+                  hundreds of options, MoodBrew ensures you find the perfect
+                  match for your mood, providing a personalized and enriching
+                  tea experience.
+                </Accordion.Panel>
+              </Accordion.Item>
 
-                <Accordion.Item value="quality">
-                  <Accordion.Control
-                    icon={
-                      <IconUserQuestion
-                        style={{
-                          color: "var(--taupe)",
-                          width: rem(20),
-                          height: rem(20),
-                        }}
-                      />
-                    }
-                  >
-                    How Do We Ensure the Quality and Safety of the Teas Recommended?
-                  </Accordion.Control>
-                  <Accordion.Panel>Content</Accordion.Panel>
-                </Accordion.Item>
-              </Stack>
-            </Flex>
-          </Accordion>
-        </Flex>
-      </div>
+              <Accordion.Item value="quality">
+                <Accordion.Control
+                  icon={
+                    <IconUserQuestion
+                      style={{
+                        color: "var(--taupe)",
+                        width: rem(20),
+                        height: rem(20),
+                      }}
+                    />
+                  }
+                >
+                  <Text fw={500} c="taupe" pr="md">
+                    How Do We Ensure the Quality and Safety of the Teas
+                    Recommended?
+                  </Text>
+                </Accordion.Control>
+                <Accordion.Panel
+                  style={{
+                    maxWidth: "600px",
+                    overflow: "auto",
+                    paddingLeft: "2rem",
+                    paddingright: "3rem",
+                  }}
+                >
+                  To ensure the quality and safety of the teas recommended by
+                  MoodBrew, we adhere to stringent standards and practices at
+                  every stage of the process. Here's how we guarantee the
+                  excellence of our teas
+                </Accordion.Panel>
+              </Accordion.Item>
+            </Stack>
+          </Flex>
+        </Accordion>
+      </Flex>
+    </div>
   );
 }
 
