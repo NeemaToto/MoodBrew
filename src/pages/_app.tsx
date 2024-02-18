8576eb7ff2a4d24c4f959a88664df81da1e0eb28
Neema Totonchi <neematotonchi@Neemas-MacBook-Pro.local>
2024-02-17 18:50:20 -0800

    add local font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,9 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import '@mantine/core/styles.css';
 import { createTheme, MantineProvider, rem } from '@mantine/core';
+import localFont from 'next/font/local'
+
+const satoshi = localFont({ src: '/satoshi/Satoshi.ttf' })
 
 const theme = createTheme({
   primaryColor: "sage",
@@ -11,7 +14,7 @@ const theme = createTheme({
     dun: ["#fff4e8", "#f5e7d8", "#e7cdb4", "#d8b28b", "#cd9a69", "#c58b53", "#c28446", "#ab7138", "#99652e", "#865624"],
     seasalt: ["#f3f3f6", "#e4e4e6", "#c7c7cc", "#a8a8b5", "#8d8da0", "#7c7c93", "#F7F7F9", "#62627b", "#57566f", "#4a4a63"]
   },
-  fontFamily: 'Satoshi, sans-serif',
+  fontFamily: satoshi.style.fontFamily,
   headings: {
     sizes: {
       h1: {
@@ -45,7 +48,7 @@ const theme = createTheme({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider theme={theme}>
-      <Component {...pageProps} />
+      <Component className={satoshi.className} {...pageProps} />
     </MantineProvider>
   )
 }
