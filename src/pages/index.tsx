ae1f721388813326ddcfab42477b23a88b849e75
KushalK04 <Kushal06Kumar@gmail.com>
2024-02-18 22:49:57 -0800

    phui sucks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,33 @@
 import { Button, Text, Stack, Title, rem } from '@mantine/core'
 import FAQ from '@/components/FAQ'
 import TeaCard from '@/components/teaCard'
+import Image from 'next/image'
 import classes from '@/styles/Home.module.css'
+import { NavbarSearch } from './searchBar'
 
 export default function Home() {
   return (
-      <main className={``}>
-        <Stack gap="sm">
+    <main className={``}>
+      <Stack gap="sm">
+        <div>
           <Title c="taupe" order={1} fw={800}>
-            Brew Your Mood
+            Mood Brew
           </Title>
           <Text c="taupe" size='lg'>
-          Explore Over 100 Unique Tea Suggestions Tailored for Health and Mood.
+            Explore Over 100 Unique Tea Suggestions Tailored for Health and Mood.
           </Text>
-        </Stack>
-        <FAQ />
-      </main>
+        </div>
+        <div>
+          {/* <img src="/Ellipse 1.jpg"/> */}
+        </div>
+      </Stack>
+      <div>
+        <div className=''>
+          <NavbarSearch/>
+        </div>
+      </div>
+      <FAQ />
+    </main>
 
   )
 }
---
+++ src/pages/searchBar.tsx
@@ -0,0 +1,30 @@
+import {
+    TextInput,
+    Code,
+    rem,
+  } from '@mantine/core';
+  import { IconSearch } from '@tabler/icons-react';
+  import classes from '../styles/NavbarSearch.module.css';
+  
+  
+
+  
+  export function NavbarSearch() {
+  
+  
+    return (
+      <div className={classes.bghome}>
+        <TextInput
+        className={classes.input}
+          placeholder="Search"
+          size="xl"
+          leftSection={<IconSearch style={{ width: rem(12), height: rem(12), marginLeft: rem(90) }} stroke={1.5} />}
+          rightSectionWidth={70}
+          rightSection={<Code className={classes.searchCode}><img src="/arrow.png" width={20} height={20}/></Code>}
+          styles={{ section: { pointerEvents: 'none' } }}
+          mb="sm"
+        />
+
+      </div>
+    );
+  }
